937cc4424c9a3637604f22852147c6a56c1353b1
rohith2341 <95401519+rohith2341@users.noreply.github.com>
2024-05-10 17:22:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header";
+import Todo from "./components/Todo";
+
+
+function App() {
+
+  return ( 
+  <div className="App"> 
+    <Header/>
+    <Todo/> 
+  </div>
+  );
+ 
+}
+
+export default App;
+
+
